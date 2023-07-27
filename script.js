325f3e8f0a756b158f08ebb120cf6362a54a79bd
Dawid Kowalski <dawid@example.com>
2023-07-27 12:47:35 +0200

    not yet done 3

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let diskOne = document.querySelector(".disk1");
-let diskTwo = document.querySelector(".disk2");
-let diskThree = document.querySelector(".disk3");
-diskOne.addEventListener("click", function () {
-  document.addEventListener(
-    "mousemove",
-    function (ev) {
-      diskOne.style.transform = "translateY(" + (ev.clientY - 80) + "px)";
-      diskOne.style.transform += "translateX(" + (ev.clientX - 100) + "px)";
-    },
-    false
-  );
-});
+let disks = document.querySelectorAll(".disk");
+let boxOne = document.querySelector(".box1");
+let boxTwo = document.querySelector(".box2");
+let boxThree = document.querySelector(".box3");
+
+//
+for (disk of disks) {
+  disk.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    boxTwo.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    boxThree.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    boxOne.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    boxOne.addEventListener("drop", function (e) {
+      boxOne.appendChild(selected);
+      selected = null;
+    });
+    boxTwo.addEventListener("drop", function (e) {
+      boxTwo.appendChild(selected);
+      selected = null;
+    });
+    boxThree.addEventListener("drop", function (e) {
+      boxThree.appendChild(selected);
+      selected = null;
+    });
+  });
+}
