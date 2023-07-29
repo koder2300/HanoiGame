79a86b824356efe45a3a1e6a6b5bb5b9141b131e
Dawid Kowalski <dawid@example.com>
2023-07-29 22:32:05 +0200

    giving result of game 1

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 let disks = document.querySelectorAll(".disk");
 let numberMoves = 0;
 let numbers = document.querySelector(".number");
+numbers.textContent = "0";
+let draggable = [];
 //
+let disk1 = document.querySelector(".disk1");
+let disk2 = document.querySelector(".disk2");
+let disk3 = document.querySelector(".disk3");
+//
+let red1 = document.querySelector(".red");
+let diskOne = document.querySelector("#disk1");
+let diskTwo = document.querySelector("#disk2");
+let diskThree = document.querySelector("#disk3");
+let blue1 = document.querySelector(".blue");
+let green1 = document.querySelector(".green");
+let box3 = document.querySelector(".box:nth-child(3)");
+// console.log(box3);
 //      FIRST RESULT
 let boxes = document.querySelectorAll(".box");
-disks.forEach((disk) => {
-  disk.addEventListener("dragstart", () => {
-    disk.classList.add("dragging");
-  });
-  disk.addEventListener("dragend", (e) => {
-    disk.classList.remove("dragging");
-    numberMoves++;
-    numbers.textContent = `${numberMoves}`;
+diskOne.addEventListener("dragstart", () => {
+  diskOne.classList.add("dragging");
+  console.log(`yes`);
+
+  disks.forEach((disk) => {
+    disk.addEventListener("dragstart", () => {
+      disk.classList.add("dragging");
+    });
+
+    disk.addEventListener("dragend", (e) => {
+      disk.classList.remove("dragging");
+      numberMoves++;
+      numbers.textContent = `${numberMoves}`;
+      draggable.push(disk);
+
+      //
+      if (
+        box3.contains(diskOne) &&
+        box3.contains(diskTwo) &&
+        box3.contains(diskThree)
+      ) {
+        if (
+          draggable.indexOf(red1) === 0 &&
+          draggable.indexOf(blue1) === 1 &&
+          draggable.indexOf(green1) === 2
+        ) {
+          alert(`Koniec gry !! Wygrałeś w ${numberMoves} posunięciach`);
+        }
+      }
+      //
+    });
   });
 });
 boxes.forEach((box) => {
@@ -22,6 +59,8 @@ boxes.forEach((box) => {
   });
 });
 
+//
+// if disk3 jest w box 3 &&box 1jest w box 3 &&
 ////////// MATI RESULT
 
 //////////////////////    MATI RESULT
